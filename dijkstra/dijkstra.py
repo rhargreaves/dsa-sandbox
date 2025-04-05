2f923a03ed5e8b88ff894d7861944dea96f2be09
Robert Hargreaves <kiyouta@gmail.com>
2025-04-05 17:55:07 +0100

    Keep track and return shortest path taken

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -21,29 +21,33 @@ class Connection:
 
 def find_shortest_path(startNode, goalNode):
     visited = []
+    path = dict({})  # currentNodeLabel, sourceNode
 
     def walk(node):
         visited.append(node)
         print(f"*** Visited Node {node.label} weight = {node.weight}\n")
 
         for c in node.neighbours:
+            if c in visited:
+                print(f"*** Skipping weight check of {c.node.label} as it is visited already")
+                continue
             new_weight = c.effort + node.weight
             if new_weight < c.node.weight:
                 c.node.weight = new_weight
-                print(f"*** Node {c.node.label} weight = {c.node.weight}\n")
+                print(f"*** NEW Node {c.node.label} weight = {c.node.weight} from = {node.label}\n")
+                path[c.node.label] = node
             else:
                 print(f"*** Not updating node weight: {c.node.label} weight = {c.node.weight}\n")
 
         for c in sorted(node.neighbours, key=lambda c: c.node.weight):
-            walk(c.node)
+            if c.node not in visited:
+                walk(c.node)
 
     walk(startNode)
 
-    path = sorted(visited, key=lambda n: n.weight)
-    result = []
-    for n in path:
-        print(f"--- Node: {n}\n")
-        result.append(n)
-        if n == goalNode:
-            break
-    return [n.label for n in result]
+    node = goalNode
+    pathBack = []
+    while node is not None:
+        pathBack.append(node.label)
+        node = path.get(node.label, None)
+    return list(reversed(pathBack))
