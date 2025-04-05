e7b6b86d45d0ac9e8f8c2ae8d372ca572deff179
Robert Hargreaves <kiyouta@gmail.com>
2025-04-05 15:06:06 +0100

    Test 3 nodes all in a line

--- dijkstra/dijkstra_test.py
+++ dijkstra/dijkstra_test.py
@@ -15,3 +15,12 @@ def test_two_nodes():
 
     path = find_shortest_path(nodes)
     assert path == ['START', 'GOAL']
+
+
+def test_three_nodes_one_path():
+    nodes = Node('START', 0, [
+        Node('A', 2, [
+            Node('GOAL', 2, None)])])
+
+    path = find_shortest_path(nodes)
+    assert path == ['START', 'A', 'GOAL']
