57ec6e124a6524f85b7196eea3fd4ff8330cc57e
Robert Hargreaves <kiyouta@gmail.com>
2025-04-05 20:07:17 +0100

    Implemented using heapq as a min-heap/priority queue

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from heapq import heappop, heappush
 import sys
 
 INF = sys.maxsize
@@ -11,6 +12,9 @@ class Node:
     neighbours: list['Connection']
     prev: 'Node' = None   # optional: for tracking back to build the shortest path!
 
+    def __lt__(self, other):
+        self.weight < other.weight
+
 
 @dataclass
 class Connection:
@@ -20,19 +24,16 @@ class Connection:
 
 def find_shortest_path(startNode, goalNode) -> tuple[int, list[str]]:
     visited = []
-
     seenNodes = []
-
-    def get_lowest_weight_node_seen():
-        nodes = list(sorted(seenNodes, key=lambda n: n.weight))
-        if len(nodes) == 0:
-            return None
-        return nodes[0]
 
     def walk(node):
         visited.append(node.label)
         print(f"*** Visited Node {node.label} from {
             node.prev.label if node.prev is not None else "unknown"} weight = {node.weight}\n")
+
+        if node == goalNode:
+            print("*** Visited goal node. Ending.")
+            return
 
         for c in node.neighbours:
             if c.node.label in visited:
@@ -45,15 +46,11 @@ def find_shortest_path(startNode, goalNode) -> tuple[int, list[str]]:
                       f"arriving from {node.label}\n")
                 # optional: recreate the path taken by tracking previous node when weight is updated
                 c.node.prev = node
-                if c.node not in seenNodes:
-                    seenNodes.append(c.node)
+                heappush(seenNodes, (c.node.weight, c.node))
             else:
                 print(f"*** Not updating node weight: {c.node.label} weight = {c.node.weight}\n")
 
-        nextNode = get_lowest_weight_node_seen()
-        if nextNode is None:
-            return
-        seenNodes.remove(nextNode)
+        nextNode = heappop(seenNodes)[1]
         walk(nextNode)
 
     walk(startNode)
