315d695722534bd041f346b7ab1aae03a2b48024
Robert Hargreaves <kiyouta@gmail.com>
2025-04-05 22:49:34 +0100

    Basic adder

--- full_adder/adder.py
+++ full_adder/adder.py
@@ -1,3 +1,9 @@
 
 def add(x, y):
-	return x + y
+    return x + y
+
+
+def add_bits(a: int, b: int, c_in: int) -> tuple[int, int]:   # sum, c_out
+    sum = a ^ b ^ c_in
+    c_out = (a & b) | (b & c_in) | (a & c_in)
+    return sum, c_out
--- full_adder/adder_test.py
+++ full_adder/adder_test.py
@@ -1,5 +1,22 @@
-from adder import add
+from adder import add, add_bits
 
 
 def test_adder_adds_two_numbers():
-	assert add(1, 2) == 3
+    assert add(1, 2) == 3
+
+
+def test_single_bit_adder():
+    assert add_bits(0, 1, 0) == (1, 0)
+    assert add_bits(1, 0, 0) == (1, 0)
+
+
+def test_single_bit_adder_overflow():
+    assert add_bits(1, 1, 0) == (0, 1)
+
+
+def test_single_bit_adder_zero():
+    assert add_bits(0, 0, 0) == (0, 0)
+
+
+def test_single_bit_overflow_and_carry():
+    assert add_bits(1, 1, 1) == (1, 1)
