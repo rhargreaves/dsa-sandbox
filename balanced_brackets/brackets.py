177e03046a179463c990e6825f1a6cc1be167c0a
Robert Hargreaves <kiyouta@gmail.com>
2025-04-08 00:05:54 +0100

    Add better solutions for the brackets exercise

--- balanced_brackets/brackets.py
+++ balanced_brackets/brackets.py
@@ -16,6 +16,8 @@ import sys
 # The function accepts STRING s as parameter.
 #
 
+# original solution
+
 
 def isBalanced(s):
     tokens = []
@@ -29,6 +31,46 @@ def isBalanced(s):
             if (c == ")" and popped_c != "(") or \
                 (c == "]" and popped_c != "[") or \
                     (c == "}" and popped_c != "{"):
+                return "NO"
+    return "YES" if len(tokens) == 0 else "NO"
+
+
+# alternative solution
+def isBalanced(s):
+    tokens = []
+    matched_pairs = {
+        '(': ')',
+        "{": "}",
+        "[": "]"
+    }
+    for c in s:
+        if c in "([{":
+            tokens.append(c)
+        if c in ")]}":
+            if len(tokens) == 0:
+                return "NO"
+            opener = tokens.pop()
+            if opener in matched_pairs and matched_pairs[opener] != c:
+                return "NO"
+    return "YES" if len(tokens) == 0 else "NO"
+
+
+# better solution
+def isBalanced(s):
+    tokens = []
+    matched_pairs = {
+        '(': ')',
+        "{": "}",
+        "[": "]"
+    }
+    for c in s:
+        if c in matched_pairs.keys():
+            tokens.append(c)
+        if c in matched_pairs.values():
+            if len(tokens) == 0:
+                return "NO"
+            opener = tokens.pop()
+            if opener in matched_pairs and matched_pairs[opener] != c:
                 return "NO"
     return "YES" if len(tokens) == 0 else "NO"
 
