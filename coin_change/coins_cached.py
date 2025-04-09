6689a9d84b4e740116df8a9d2656cd34768354a5
Robert Hargreaves <kiyouta@gmail.com>
2025-04-09 22:28:27 +0100

    Refactor

--- coin_change/coins_cached.py
+++ coin_change/coins_cached.py
@@ -17,27 +17,22 @@ import sys
 
 cache = {}
 
+def getWaysCached(amount, denoms):
+    key = f"{amount}_{denoms[0] if denoms else None}"
+    if key in cache:
+        return cache[key]
+    else:
+        ways = getWays(amount, denoms)
+        cache[key] = ways
+        return ways
+
 def getWays(amount, denoms):
     if amount == 0:
         return 1
     if amount < 0 or len(denoms) == 0:
         return 0
+    return getWaysCached(amount - denoms[0], denoms) + getWaysCached(amount, denoms[1:])
 
-    key = f"{amount - denoms[0]}_{denoms[0] if denoms else None}"
-    if key in cache:
-        remaining_ways = cache[key]
-    else:
-        remaining_ways = getWays(amount - denoms[0], denoms)
-        cache[key] = remaining_ways
-
-    key = f"{amount}_{denoms[1:][0] if denoms[1:] else None}"
-    if key in cache:
-        other_coin_ways = cache[key]
-    else:
-        other_coin_ways = getWays(amount, denoms[1:])
-        cache[key] = other_coin_ways
-
-    return remaining_ways + other_coin_ways
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
