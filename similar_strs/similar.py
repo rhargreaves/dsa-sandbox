5de1243b360ae9a17640653511dd9b16582439a1
Robert Hargreaves <kiyouta@gmail.com>
2025-04-09 19:27:06 +0100

    Basic implementation

--- similar_strs/similar.py
+++ similar_strs/similar.py
@@ -1,4 +1,11 @@
 
 
 def indicesToRemove(str1, str2):
-	return [-1]
+    indices = []
+    for i in range(len(str1)):
+        if str1[:i] + str1[i+1:] == str2:
+            indices.append(i)
+
+    if not indices:
+        return [-1]
+    return indices
--- similar_strs/similar_test.py
+++ similar_strs/similar_test.py
@@ -6,3 +6,10 @@ def test_returns_minus_1_array_if_strings_different():
     str2 = "dog"
 
     assert indicesToRemove(str1, str2) == [-1]
+
+
+def test_returns_one_index_to_remove():
+    str1 = "hind"
+    str2 = "hid"
+
+    assert indicesToRemove(str1, str2) == [2]
