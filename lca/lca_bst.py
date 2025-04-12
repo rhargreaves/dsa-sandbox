07860b536baaf39ec0da14c0aca9baa57361eb24
Robert Hargreaves <kiyouta@gmail.com>
2025-04-12 19:44:48 +0100

    Add Binary Tree Search LCA example

---
+++ lca/lca_bst.py
@@ -0,0 +1,79 @@
+import sys
+
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info):
+          self.info = info
+          self.left = None
+          self.right = None
+
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+# when its a binary search tree, we can use the value of the nodes to find the lca
+# if both values are greater than the current node, we move to the right child
+# if both values are less than the current node, we move to the left child
+# otherwise, the current node is the lca
+
+
+def lca(node, v1, v2):
+    if v1 > node.info and v2 > node.info:
+        return lca(node.right, v1, v2)
+    if v1 < node.info and v2 < node.info:
+        return lca(node.left, v1, v2)
+    return node
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+v = list(map(int, input().split()))
+
+ans = lca(tree.root, v[0], v[1])
+print(ans.info)
