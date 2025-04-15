7bc144252616a16f585c305b38d8974d23098094
Robert Hargreaves <kiyouta@gmail.com>
2025-04-15 16:43:47 +0100

    Better use of memory

--- dp/rod_cutting/main.py
+++ dp/rod_cutting/main.py
@@ -1,8 +1,5 @@
-# https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
-
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-
         memo = {}
 
         def solve(cuts, left, right):
@@ -12,19 +9,17 @@ class Solution:
             min_cost = None
             for c in cuts:
                 if c > left and c < right:
-                    remaining_cuts = tuple(v for v in cuts if v != c)
-
-                    if (remaining_cuts, left, c) in memo:
-                        left_cost = memo[(remaining_cuts, left, c)]
+                    if (left, c) in memo:
+                        left_cost = memo[(left, c)]
                     else:
-                        left_cost = solve(remaining_cuts, left, c)
-                        memo[(remaining_cuts, left, c)] = left_cost
+                        left_cost = solve(cuts, left, c)
+                        memo[(left, c)] = left_cost
 
-                    if (remaining_cuts, c, right) in memo:
-                        right_cost = memo[(remaining_cuts, c, right)]
+                    if (c, right) in memo:
+                        right_cost = memo[(c, right)]
                     else:
-                        right_cost = solve(remaining_cuts, c, right)
-                        memo[(remaining_cuts, c, right)] = right_cost
+                        right_cost = solve(cuts, c, right)
+                        memo[(c, right)] = right_cost
 
                     cost = left_cost + right_cost
                     min_cost = min(min_cost, cost) if min_cost is not None else cost
