90910477e9870f8b766d1b849617cf9ecfc8a664
Robert Hargreaves <kiyouta@gmail.com>
2025-04-25 23:07:26 +0100

    Refactor add two numbers

--- maths/add_two_numbers/main.py
+++ maths/add_two_numbers/main.py
@@ -12,26 +12,20 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
+
         carry = 0
-        root = ListNode()
+        root = ListNode()  # a dummy node to make the while condition easier
         node = root
-        while True:
+        while l1 or l2 or carry:
             l1_val = l1.val if l1 else 0
             l2_val = l2.val if l2 else 0
+
             sum = l1_val + l2_val + carry
-            if sum >= 10:
-                carry = 1
-                sum -= 10
-            else:
-                carry = 0
-            node.val = sum
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-            if l1 or l2 or carry:
-                node.next = ListNode()
-                node = node.next
-            else:
-                break
-        return root
+            carry = sum // 10
+            node.next = ListNode(sum % 10)
+
+            node = node.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return root.next
