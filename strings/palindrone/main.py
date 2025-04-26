1edd1885ac8a5db9f7786980063cb1b3105e368e
Robert Hargreaves <kiyouta@gmail.com>
2025-04-26 16:30:12 +0100

    Added numeric version of palindrone checker

--- strings/palindrone/main.py
+++ strings/palindrone/main.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         s = str(x)
@@ -5,4 +8,21 @@ class Solution:
             if s[i] != s[-i - 1]:
                 return False
 
+        return True
+
+    def isPalindrome_numeric_operations_only(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x == 0:
+            return True
+
+        num_digits = math.floor(math.log(x, 10)) + 1
+
+        def nth_digit(x, n):
+            y = math.pow(10, n)
+            return (x % y) // (y / 10)
+
+        for n in range(num_digits, 0, -1):
+            if nth_digit(x, n) != nth_digit(x, num_digits - n + 1):
+                return False
         return True
