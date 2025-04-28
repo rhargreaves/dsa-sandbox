20dceba9aa0932bafde9b757341078cd6d3a4644
Robert Hargreaves <kiyouta@gmail.com>
2025-04-28 12:11:02 +0100

    Add slightly cleaner version

--- sliding_window/nearby_dup/main.py
+++ sliding_window/nearby_dup/main.py
@@ -7,6 +7,20 @@ class Solution:
         if k == 0:
             return False
 
+        for i in range(len(nums)):
+            if nums[i] in pool:
+                return True
+            pool.add(nums[i])
+            if i >= k:
+                pool.remove(nums[i - k])
+
+        return False
+
+    def containsNearbyDuplicate_naive(self, nums: List[int], k: int) -> bool:
+        pool = set()
+        if k == 0:
+            return False
+
         j = 0
         for i in range(len(nums)):
             if nums[i] in pool:
