6b47eed8f7c2cf6037104906f7daa8c5fb121c28
Robert Hargreaves <kiyouta@gmail.com>
2025-04-29 23:22:14 +0100

    Add 'is subsequence' solution

---
+++ two_pointers/is_subsequence/main.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/is-subsequence/
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+
+        j = 0
+        for i in range(len(t)):
+            if t[i] == s[j]:
+                j += 1
+                if j == len(s):
+                    break
+            i += 1
+
+        return j == len(s)
