e5de1b6503f524a141e2f5d3cabf56e8bd6834e1
Robert Hargreaves <kiyouta@gmail.com>
2025-04-29 15:44:03 +0100

    Add more Nim notes

--- maths/can_win_nim/main.py
+++ maths/can_win_nim/main.py
@@ -7,3 +7,9 @@ class Solution:
 # 4 stones = LOSE (no matter what you do)
 # 5,6,7 stones = WIN (you can force opponent to get 4)
 # 8 stones = LOSE (opponent can force you to get 4)
+
+
+# SOME big clues this was solvable through some clever maths...
+# The constraint was 1 <= n <= 2^31 - 1. That's a lot of numbers to test!
+
+# The pattern became clear when I just walked through the first 8 numbers.
