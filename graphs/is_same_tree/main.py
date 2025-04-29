224a16f2a7a4d3784b710c82e30d801d328ebcf4
Robert Hargreaves <kiyouta@gmail.com>
2025-04-29 18:49:20 +0100

    Add solution for is same tree

---
+++ graphs/is_same_tree/main.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if (p and not q) or (q and not p):
+            return False
+
+        if p.val != q.val:
+            return False
+
+        if not self.isSameTree(p.left, q.left):
+            return False
+
+        if not self.isSameTree(p.right, q.right):
+            return False
+
+        return True
