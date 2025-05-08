bb85cb28c16cbe63d5d4b9b8d05cc960dfb57b4f
Robert Hargreaves <kiyouta@gmail.com>
2025-05-08 11:58:22 +0100

    Add sneaky XOR solution for finding the difference

---
+++ bit_manipulation/find_the_diff/main.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/find-the-difference
+
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        result = 0
+        for char in s + t:
+            result ^= ord(char)
+        return chr(result)
