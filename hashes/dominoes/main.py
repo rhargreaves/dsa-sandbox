2455c9cf9abd3fdb5d9e739d31a22340ef0caa17
Robert Hargreaves <kiyouta@gmail.com>
2025-05-09 14:41:58 +0100

    Simplify

--- hashes/dominoes/main.py
+++ hashes/dominoes/main.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from typing import List
 
 # https://leetcode.com/problems/number-of-equivalent-domino-pairs
@@ -6,19 +5,14 @@ from typing import List
 
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-
-        def key(pair):
-            if pair[0] < pair[1]:
-                return (pair[0], pair[1])
-            else:
-                return (pair[1], pair[0])
-
-        seen = defaultdict(lambda: 0)
+        seen = {}
         pairs = 0
         for pair in dominoes:
-            if key(pair) in seen:
-                seen[key(pair)] += 1
-                pairs += seen[key(pair)]
+            key = tuple(sorted(pair))
+
+            if key in seen:
+                seen[key] += 1
+                pairs += seen[key]
             else:
-                seen[key(pair)] = 0
+                seen[key] = 0
         return pairs
