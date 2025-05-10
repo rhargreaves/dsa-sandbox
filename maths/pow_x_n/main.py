59b80971bba8fd339caa90c89f16bafe57ea0dc7
Robert Hargreaves <kiyouta@gmail.com>
2025-05-10 17:07:50 +0100

    Math trickery

--- maths/pow_x_n/main.py
+++ maths/pow_x_n/main.py
@@ -5,11 +5,11 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
-            return 1
+            return 1  # x^0 == 1
         if n < 0:
             n = -n
-            x = 1 / x
+            x = 1 / x  # x^-n == 1 / x^n
         if n % 2 == 0:
-            return self.myPow(x * x, n / 2)
+            return self.myPow(x * x, n / 2)  # (x^2)^(n/2) == x^n
         else:
-            return x * self.myPow(x, n - 1)
+            return x * self.myPow(x, n - 1)  # x * x^(n-1) == x^n
